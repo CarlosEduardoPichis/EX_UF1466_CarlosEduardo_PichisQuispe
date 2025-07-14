d4a651cfda5da941be3a85767dcb37365b3ad9a9
Carlos Eduardo <a21carpicqui@inspedralbes.cat>
2025-07-14 12:31:37 +0200

    modificacion del bucle

--- Ex_03/Alarma.py
+++ Ex_03/Alarma.py
@@ -5,7 +5,7 @@ def alarm_simulator(total_time):
     Variable current_second: incrementa en 1 para simular el paso de un segundo.
     Sentencia continue: para pausar la alarma cada 10 segundos.
     """
-    while True:
+    while current_second < total_time:
         current_second += 1
         if current_second % 10 == 0:
             print(f"Alarma pausada en el segundo {current_second}")
